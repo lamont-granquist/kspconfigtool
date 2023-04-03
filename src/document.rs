ec33e4bc48773b3a48dc68efde7039f87936c858
Lamont Granquist <lamont@scriptkiddie.org>
2023-04-03 14:10:33 -0700

    correctly preserves line endings
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- src/document.rs
+++ src/document.rs
@@ -13,50 +13,56 @@ pub enum Statement {
 
 impl Document {
     pub fn to_windows(&self) -> String {
-        let mut buf = String::new();
-        let mut f = core::fmt::Formatter::new(&mut buf);
+        let mut builder = string_builder::Builder::default();
 
         for stmt in &self.statements {
-            _fmt(&mut f, stmt, 0).expect("a Display implementation returned an error unexpectedly");
+            _string(&mut builder, stmt, 0, "\r\n");
         }
 
-        buf
+        builder.string().unwrap()
     }
 }
 
 impl fmt::Display for Document {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut builder = string_builder::Builder::default();
+
         for stmt in &self.statements {
-            _fmt(f, stmt, 0)?
+            _string(&mut builder, stmt, 0, "\n");
         }
-        Ok(())
+
+        write!(f, "{}", builder.string().unwrap())
     }
 }
 
 impl fmt::Display for Statement {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        _fmt(f, &self, 0)
+        let mut builder = string_builder::Builder::default();
+
+        _string(&mut builder, &self, 0, "\n");
+
+        write!(f, "{}", builder.string().unwrap())
     }
 }
 
-fn _fmt_node(f: &mut fmt::Formatter<'_>, identifier: &str, statements: &[Statement], depth: usize) -> fmt::Result {
+fn _string_node(builder: &mut string_builder::Builder, identifier: &str, statements: &[Statement], depth: usize, ending: &str) {
     let indent: String = std::iter::repeat("\t").take(depth).collect();
-    writeln!(f, "{}{}", indent, identifier)?;
-    writeln!(f, "{}{{", indent)?;
+    builder.append(format!("{}{}{}", indent, identifier, ending));
+    builder.append(format!("{}{{{}", indent, ending));
     for stmt in statements {
-        _fmt(f, stmt, depth+1)?;
+        _string(builder, stmt, depth+1, ending);
     }
-    writeln!(f, "{}}}", indent)
+    builder.append(format!("{}}}{}", indent, ending));
 }
 
-fn _fmt_assignment(f: &mut fmt::Formatter<'_>, identifier: &str, value: &str, depth: usize) -> fmt::Result {
+fn _string_assignment(builder: &mut string_builder::Builder, identifier: &str, value: &str, depth: usize, ending: &str) {
     let indent: String = std::iter::repeat("\t").take(depth).collect();
-    writeln!(f, "{}{} = {}", indent, identifier, value)
+    builder.append(format!("{}{} = {}{}",indent, identifier, value, ending));
 }
 
-fn _fmt(f: &mut fmt::Formatter<'_>, statement: &Statement, depth: usize) -> fmt::Result {
+fn _string(builder: &mut string_builder::Builder, statement: &Statement, depth: usize, ending: &str) {
     match statement {
-        Statement::Assignment(identifier, value) => _fmt_assignment(f, identifier, value, depth),
-        Statement::Node(identifier, statements) => _fmt_node(f, identifier, statements, depth),
+        Statement::Assignment(identifier, value) => _string_assignment(builder, identifier, value, depth, ending),
+        Statement::Node(identifier, statements) => _string_node(builder, identifier, statements, depth, ending),
     }
 }
